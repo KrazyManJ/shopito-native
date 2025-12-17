a72bf4aee25b673101596bf277a2fb9da593a3c0
KrazyManJ <KrazyManJ@gmail.com>
2025-12-17 02:29:43 +0100

    Added basic map view

--- app/(tabs)/map_view.tsx
+++ app/(tabs)/map_view.tsx
@@ -1,5 +1,25 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function Tab() {
-    return <View></View>;
+    return (
+        <View style={styles.container}>
+            <MapView style={styles.map}>
+                <Marker
+                    coordinate={{ latitude: 50, longitude: 49 }}
+                    title="My cool marker"
+                />
+            </MapView>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: "100%",
+        height: "100%",
+    },
+});
